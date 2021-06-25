a6870a820ed67914aa5105c03066a1169c08ad56
Prabhav Thali <Prabhav.Thali1@ibm.com>
2021-06-25 14:25:24 +0530

    Update build_httpd.sh

--- build_httpd.sh
+++ build_httpd.sh
@@ -156,7 +156,7 @@ case "$DISTRO" in
 "rhel-8.2" | "rhel-8.3" | "rhel-8.4")
   printf -- "Installing %s %s for %s \n" "$PACKAGE_NAME" "$PACKAGE_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
   printf -- 'Installing the dependencies for HTTP server from repository \n' |& tee -a "$LOG_FILE"
-  sudo yum install -y git openssl openssl-devel python2 gcc libtool autoconf make pcre pcre-devel libxml2 libxml2-devel expat-devel which wget tar |& tee -a "$LOG_FILE"
+  sudo yum install -y git openssl openssl-devel python2 gcc libtool autoconf make pcre pcre-devel libxml2 libxml2-devel expat-devel diffutils which wget tar |& tee -a "$LOG_FILE"
   configureAndInstall |& tee -a "$LOG_FILE"
   ;;
 

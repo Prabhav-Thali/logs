9db8caf9637b03b77d088e7f938659327c48af0f
Prabhav Thali <Prabhav.Thali1@ibm.com>
2022-09-30 14:15:50 +0530

    Update build_ruby.sh

--- build_ruby.sh
+++ build_ruby.sh
@@ -90,7 +90,7 @@ function configureAndInstall()
   unzip bazel-$PACKAGE_VERSION-dist.zip
   chmod -R +w .
   
-  curl -sSL $PATCH/bazel.patch | git apply -v
+  curl -sSL $PATCH/bazel.patch | git apply -v --unsafe-paths
 }
 
 function logDetails()
